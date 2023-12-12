b412d0678d78b9346439f9f5f8ef28ae1c209bdd
Gabriel Gómez <85773507+GabrielG-PWD@users.noreply.github.com>
2023-12-12 12:01:05 -0300

    agregando mas modulos y componentes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../components/sidebar/sidebar.component';
 
-
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
