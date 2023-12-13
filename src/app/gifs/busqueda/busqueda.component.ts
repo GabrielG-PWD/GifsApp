7603c1e941ed9278bce97cb768780cfe8bf88768
Gabriel Gómez <85773507+GabrielG-PWD@users.noreply.github.com>
2023-12-13 15:54:19 -0300

    trabajando el historial

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,9 +10,11 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor( private gifsService: GifsService ) { }
+
   buscar() {
-    let valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
 }
